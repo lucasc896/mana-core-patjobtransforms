83ab211763ca41643ac984ab059838e455661ed0
kama <kama@4525493e-7705-40b1-a816-d608a930855b>
2011-05-04 19:07:15 +0000

    Modified skeleton.DQHistogramMerge_trf.py to use subprocess module
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/PATJobTransforms/trunk@363756 4525493e-7705-40b1-a816-d608a930855b

--- share/skeleton.DQHistogramMerge_trf.py
+++ share/skeleton.DQHistogramMerge_trf.py
@@ -15,13 +15,31 @@ for f in inFiles:
     mergeListFile.write( str(f) + '\n' )    
 mergeListFile.close()
 
+#
+#
+# This is the old method which was causing stray DQHistogramMerge processes.
+#
 # call DQHistogramMerge
-cmd = 'DQHistogramMerge.py hist_merge_list.txt %s False' % (runArgs.outputHISTFile)
-import commands
-(status, output) = commands.getstatusoutput(cmd)
+#cmd = 'DQHistogramMerge.py hist_merge_list.txt %s False' % (runArgs.outputHISTFile)
+#cmd = 'sleep 500' 
+#import commands
+#(status, output) = commands.getstatusoutput(cmd)
+
+import os,subprocess
+#writing in a temp file should allow termination eventually even if parent is killed
+tmpbuff=os.tmpfile()
+dqhistpipe=subprocess.Popen(["DQHistogramMerge.py", "hist_merge_list.txt", runArgs.outputHISTFile, "False"],
+                            stdout=tmpbuff, stderr=tmpbuff, shell=False)
+status=dqhistpipe.wait()
+
 print "---------------------------------------------------------------------------------------"
-print '## Output of \'' + cmd + '\':'
-print output
+print '## Output of \'DQHistogramMerge.py hist_merge_list.txt '  + runArgs.outputHISTFile + ' False\':'
+try:
+    tmpbuff.seek(0)
+    for line in tmpbuff:
+        print line,
+finally:
+    tmpbuff.close()
 print '## DQHistogramMerge.py finished with retcode = %s' % (status)
 print "---------------------------------------------------------------------------------------"
 
