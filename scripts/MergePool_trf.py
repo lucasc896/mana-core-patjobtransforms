25fb37a87d67bdecc8250fb0c9ca0077bdcf6924
sbeale <sbeale@4525493e-7705-40b1-a816-d608a930855b>
2012-02-13 14:49:18 +0000

    Updated fastPoolMerge to use new exit codes
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/PATJobTransforms/trunk@482392 4525493e-7705-40b1-a816-d608a930855b

--- scripts/MergePool_trf.py
+++ scripts/MergePool_trf.py
@@ -44,7 +44,12 @@ class MergePoolJobTransform( JobTransform ):
           if line:
             print "mergePOOL.exe Report: %s" % line.strip()
         rc = p.returncode
-        print "mergePOOL finished with code %s" % rc
+        print "1st mergePOOL (event data) finished with code %s" % rc
+        if rc == 1:
+            print "mergePOOL.exe finished with unknown status"
+        elif rc > 1:
+            raise TransformError("mergePOOL.exe (event merge) encountered a problem",error='TRF_MERGEERR') 
+
         #2nd merge with metadata.pool to produce final output
         cmd = 'mergePOOL.exe -o events.pool.root -i %s ' % outputfile
         p = Popen(cmd,shell=True,stdout=PIPE,stderr=PIPE,close_fds=True)
@@ -53,8 +58,13 @@ class MergePoolJobTransform( JobTransform ):
           if line:
             print "mergePOOL.exe Report: %s" % line.strip()
         rc = p.returncode
-        print "mergePOOL finished with code %s" % rc
-        shutil.move('events.pool.root',outputfile)
+        print "2nd mergePOOL (metadata) finished with code %s" % rc
+        if rc == 1:
+            print "mergePOOL.exe finished with unknown status"
+        elif rc > 1:
+            raise TransformError("mergePOOL.exe (final merge) encountered a problem",error='TRF_MERGEERR') 
+        else:
+          shutil.move('events.pool.root',outputfile)
 
 
 # Python executable
